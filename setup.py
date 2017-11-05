aaa224331cbc3236bfcfb58c14e2ca6055d7549c
Ahmed Jazzar <me@ahmedjazzar.com>
2017-11-05 23:27:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,18 @@ from setuptools import setup, find_packages
 setup(
     name='django-cache-toolbox',
     description="Non-magical object caching for Django.",
-    version='0.1',
-    url='http://code.playfire.com/django-cache-toolbox',
+    version='0.2.4',
+    url='https://chris-lamb.co.uk/projects/django-cache-toolbox',
 
-    author='Playfire.com',
-    author_email='tech@playfire.com',
+    author='Chris Lamb',
+    author_email='chris@chris-lamb.co.uk',
     license='BSD',
 
-    packages=find_packages(),
+    packages=find_packages(exclude=('tests',)),
+
+    install_requires=(
+        "Django>=1.8",
+    ),
+
+    test_suite='tests',
 )
